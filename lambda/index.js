765611c73859968d4e48c4484f8b8aeae4142f72
Nicola Aitken <nicola@typeform.com>
2017-05-26 11:20:23 +0200

    Fix formatting

--- lambda/index.js
+++ lambda/index.js
@@ -1,34 +1,33 @@
-var http = require("https");
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-var encoded_gb_key = ""
+var http = require("https"),
+    events = require("events"),
+    eventEmitter = new events.EventEmitter(),
+    encoded_gb_key = "";
 
 if (process.env.GB_KEY) {
-    encoded_gb_key = new Buffer(process.env.GB_KEY).toString('base64')
-
+    encoded_gb_key = new Buffer(process.env.GB_KEY).toString("base64");
 }
 
-var handler  = (event, context, callback) => {
+var handler = (event, context, callback) => {
 
-    eventEmitter.on('data_defined', () => {
-        data = transformData(event.form_response)
-        postToGeckoboardAPI(data, "/datasets/" + datasetName + "/data", "data_sent")
+    eventEmitter.on("data_defined", () => {
+        data = transformData(event.form_response);
+        postToGeckoboardAPI(data, "/datasets/" + datasetName + "/data", "data_sent");
     });
 
-    eventEmitter.on('data_sent', () => {
-        let response = {
-            statusCode: '200',
-            body: JSON.stringify({ message: 'Successfully sent data to Geckoboard!' }),
+    eventEmitter.on("data_sent", () => {
+        var response = {
+            statusCode: "200",
+            body: JSON.stringify({ message: "Successfully sent data to Geckoboard!" }),
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             }
         };
-    context.succeed(response);
-    })
+        context.succeed(response);
+    });
 
-    datasetName = getDatasetName(event.form_response.definition)
-    definition = defineData(event.form_response.definition.fields)
-    postToGeckoboardAPI(definition, "/datasets/" + datasetName, "data_defined")
+    datasetName = getDatasetName(event.form_response.definition);
+    definition = defineData(event.form_response.definition.fields);
+    postToGeckoboardAPI(definition, "/datasets/" + datasetName, "data_defined");
 };
 
 var defineData = (fields) => {
@@ -39,20 +38,20 @@ var defineData = (fields) => {
                 "name": "Submit date"
             }
         },
-        "unique_by": ["submitted_at"]
-    }
+        "unique_by": ["submitted_at"],
+    };
     fields.forEach(function (field) {
         definition.fields[field.id.toLowerCase()] = {
             "type": getFieldDataType(field),
             "name": field.title
-        }
+        };
     });
-    return definition
-}
+    return definition;
+};
 
 var getDatasetName = (formDefinition) => {
-    return formDefinition.title.replace(" ", ".");
-}
+    return formDefinition.title.toLowerCase().replace(" ", ".");
+};
 
 var getFieldDataType = (field) => {
     switch (field.type) {
@@ -67,34 +66,34 @@ var getFieldDataType = (field) => {
         default:
             return "string";
     }
-}
+};
 
 var transformData = (formResponse) => {
-        date = new Date()
-        data = [{
-            "submitted_at": formResponse.submitted_at
-        }]
+    date = new Date();
+    data = [{
+        "submitted_at": formResponse.submitted_at
+    }];
     formResponse.answers.forEach(function(answer) {
         fieldRef = answer.field.id.toLowerCase();
-        data[0][fieldRef] = extractAnswer(answer)
+        data[0][fieldRef] = extractAnswer(answer);
     });
     return { "data": data };
 };
 
 var extractAnswer = (answerData) => {
     switch (answerData.type) {
-        case 'boolean':
-            return answerData.boolean ? "yes" : "no"
-        case 'choice':
-           return answerData.choice.label
+        case "boolean":
+            return answerData.boolean ? "yes" : "no";
+        case "choice":
+            return answerData.choice.label;
         default:
-           return answerData[answerData.type]
+            return answerData[answerData.type];
     }
 };
 
 var postToGeckoboardAPI = function(payload, path, eventName) {
-    var method = eventName === 'data_defined' ? "PUT" : "POST"
-    payloadString = JSON.stringify(payload)
+    var method = eventName === "data_defined" ? "PUT" : "POST";
+    payloadString = JSON.stringify(payload);
     var options = {
         "method": method,
         "hostname": "api.geckoboard.com",
@@ -102,7 +101,7 @@ var postToGeckoboardAPI = function(payload, path, eventName) {
         "headers": {
             "authorization": "Basic " + encoded_gb_key,
             "content-type": "application/json",
-            "content-length": Buffer.byteLength(payloadString, 'utf8')
+            "content-length": Buffer.byteLength(payloadString, "utf8")
         }
     };
 
@@ -113,27 +112,30 @@ var postToGeckoboardAPI = function(payload, path, eventName) {
             chunks.push(chunk);
         });
 
-        if(res.statusCode != '200' || '201'){
-            console.log("[ERROR] status code: " + res.statusCode)
-        }
 
-        res.on('error', function(exception) { console.log("[ERROR] from Geckoboard:" + exception); });
+        res.on("error", function(exception) {
+            console.log("[ERROR] from Geckoboard:" + exception);
+        });
 
         res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            eventEmitter.emit(eventName)
+            if (!(res.statusCode == 200 || res.statusCode == 201)) {
+                console.log("[ERROR] status code: " + res.statusCode);
+                var body = Buffer.concat(chunks);
+                console.log("[ERROR] from Geckoboard:" + body);
+            }
+            eventEmitter.emit(eventName);
         });
-    }).on('error', function(e) {
-        console.log('[ERROR] error posting data to geckoboard for' + eventName + ' :' + e.message);
+    }).on("error", function(e) {
+        console.log("[ERROR] error posting data to geckoboard for" + eventName + " :" + e.message);
     });
 
     req.write(payloadString);
     req.end();
-}
+};
 
 module.exports = {
     handler: handler,
     _defineData: defineData,
     _getDatasetName: getDatasetName,
     _extractAnswer: extractAnswer,
-}
+};
